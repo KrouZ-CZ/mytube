d594b61c7a508789b78bcfc1a34cac5c0a5de2e9
KrouZ_CZ <random1qawe@gmail.com>
2023-04-08 21:50:19 +0300

    first

---
+++ main.py
@@ -0,0 +1,14 @@
+from youtubesearchpython import *
+
+class Enige:
+    def __init__(self):
+        pass
+
+    def for_channel(self):
+        pass
+
+    def for_videos(self):
+        pass
+
+    def from_subscribes(self):
+        pass
