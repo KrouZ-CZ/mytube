ea9a094f9019748b147357e6098d6e7e6d4a9b4f
KrouZ_CZ <random1qawe@gmail.com>
2023-04-09 09:19:15 +0300

    video search

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 from youtubesearchpython import *
+from consolemenu import *
+from consolemenu.items import *
+import os
+import json
 
-class Enige:
-    def __init__(self):
-        pass
+def play_video(id_):
+    os.system(f'mpv "https://youtu.be/{id_}"')
 
-    def for_channel(self):
-        pass
+def videos_channel():
+    pass
 
-    def for_videos(self):
-        pass
+def channel_search():
+    pass
 
-    def from_subscribes(self):
-        pass
+def video_search():
+    name = input("Video name: ")
+
+    videos = VideosSearch(name)
+    menu = ConsoleMenu("Select video", "Select video to play")
+
+    for video in videos.result()["result"]:
+        v = FunctionItem(f'{video["title"]} [{video["viewCount"]["short"]}] ({video["channel"]["name"]})', play_video, [video["id"]])
+        menu.append_item(v)
+
+    menu.show()
+
+
+def main():
+    menu = ConsoleMenu("Select", "Select type of search")
+
+    search_video = FunctionItem("Video search", video_search)
+    search_channel = FunctionItem("Channel search", channel_search)
+    videos_in_channel = FunctionItem("Videos in channel", videos_channel)
+
+    menu.append_item(search_video)
+    menu.append_item(search_channel)
+    menu.append_item(videos_in_channel)
+    menu.show()
 
 if __name__ == "__main__":
-    pass
+    main()
